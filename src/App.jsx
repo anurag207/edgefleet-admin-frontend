c0cea03674cdae8bee716e812b517ce831016822
Anurag <anuragkamboj20@gmail.com>
2025-07-20 19:41:40 +0530

    Add Feature Mission Logs

--- src/App.jsx
+++ src/App.jsx
@@ -2,6 +2,7 @@ import React from 'react';
 import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
 import AdminPanel from './AdminPanel';
 import DroneDetails from "./DroneDetails";
+import MissionLogs from './Components/MissionLogs';
 
 function Home() {
   return <h1>Edgefleet Frontend</h1>;
@@ -14,6 +15,7 @@ function App() {
         <Route path="/" element={<Home />} />
         <Route path="/admin" element={<AdminPanel />} />
         <Route path="/admin/:id" element={<DroneDetails />} />
+        <Route path="/logs" element={<MissionLogs/>} />
       </Routes>
     </Router>
   );
